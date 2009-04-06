a8fcab6e2aa1672006ba28c06ee8e132b9e20de8
Leonardo Robol <robol@lab22.dm.unipi.it>
2009-04-06 18:31:22 +0200

    Defined a structure of the application.

--- songbook-editor.mm
+++ songbook-editor.mm
@@ -1,3 +1,29 @@
-<map version="0.7.1">
-<node TEXT="songbook-editor"/>
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1239034895868" ID="Freemind_Link_739180779" MODIFIED="1239034895868" TEXT="songbook-editor">
+<node CREATED="1239034934904" ID="_" MODIFIED="1239035024317" POSITION="right" TEXT="Gui (Qt4)">
+<node CREATED="1239035003332" ID="Freemind_Link_1180804855" MODIFIED="1239035014249" TEXT="Main Interface (sb_ui_main)"/>
+</node>
+<node CREATED="1239034941681" ID="Freemind_Link_1356515582" MODIFIED="1239035056122" POSITION="left" TEXT="Database manager (db_manager)">
+<node CREATED="1239035033190" ID="Freemind_Link_1712495053" MODIFIED="1239035081089" TEXT="db_handle open_db(db_file)"/>
+<node CREATED="1239035058295" ID="Freemind_Link_1940751595" MODIFIED="1239035283765" TEXT="list (of the ids) get_song_list(db_handle)"/>
+<node CREATED="1239035264263" ID="Freemind_Link_53846481" MODIFIED="1239035271810" TEXT="song get_song(song id)"/>
+</node>
+<node CREATED="1239034952871" ID="Freemind_Link_1926893403" MODIFIED="1239035426003" POSITION="right" TEXT="Latex manager (latex_manager)">
+<node CREATED="1239035294950" ID="Freemind_Link_503132419" MODIFIED="1239035385245" TEXT="string create_song(title, tone, music_author, year, text_author, body)"/>
+<node CREATED="1239035351548" ID="Freemind_Link_1399665608" MODIFIED="1239035382460" TEXT="string create latex_file(song list)"/>
+<node CREATED="1239035366322" ID="Freemind_Link_392950854" MODIFIED="1239035393247" TEXT="create_pdf( latex file , output_file)"/>
+</node>
+<node CREATED="1239035150522" ID="Freemind_Link_375303865" MODIFIED="1239035413653" POSITION="left" TEXT="Objects to code">
+<node CREATED="1239035156782" ID="Freemind_Link_287717029" MODIFIED="1239035164040" TEXT="song">
+<node CREATED="1239035166245" ID="Freemind_Link_1499777462" MODIFIED="1239035182358" TEXT="string title"/>
+<node CREATED="1239035184178" ID="Freemind_Link_1224648272" MODIFIED="1239035189216" TEXT="string body"/>
+<node CREATED="1239035195308" ID="Freemind_Link_320877804" MODIFIED="1239035200016" TEXT="string tone"/>
+<node CREATED="1239035201206" ID="Freemind_Link_243105388" MODIFIED="1239035231172" TEXT="string music_author"/>
+<node CREATED="1239035203978" ID="Freemind_Link_1251519353" MODIFIED="1239035212122" TEXT="string year"/>
+<node CREATED="1239035213050" ID="Freemind_Link_758572264" MODIFIED="1239035347723" TEXT="string text_author"/>
+<node CREATED="1239035234087" ID="Freemind_Link_1629588697" MODIFIED="1239035245966" TEXT="Methods to get this data :)"/>
+</node>
+</node>
+</node>
 </map>
