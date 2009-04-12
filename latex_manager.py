242777c038ea32ea62ceccca76cb60ae27b1f2ca
Leonardo Robol <robol@poisson.phc.unipi.it>
2009-04-12 11:24:24 +0200

    4 words instead of 3 in the chord

--- latex_manager.py
+++ latex_manager.py
@@ -14,6 +14,8 @@ class latex_manager():
         year = song.year
         body = song.body
         
+        # We really have to assume that latex_song is unicode, 
+        # or the write() function will eat us!
         latex_song = unicode()
         latex_song += "\\begin{song}{" + title + "}{" + tone + "}\n{" + mauthor + "}\n{" + tauthor + "}\n{" + year + "}" + "{}\n\n\\index{" + title + "}\n\n"
 
@@ -29,8 +31,13 @@ class latex_manager():
 
         latex_song += "\n\\end{song}\n\n"
         
+        # Transform chords in LaTeX Chords
         latex_song = re.sub("\[", "\\Ch{", latex_song)
+        latex_song = re.sub("\](\w|\s)(\w|\s)(\w|\s)(\w|\s)", self.quadra_to_chord_end, latex_song)
         latex_song = re.sub("\]", "}{}", latex_song)
         
         return latex_song
         
+    def quadra_to_chord_end(self,m):
+        st = "}{" + m.group(1) + m.group(2) + m.group(3) + m.group(4) + "}"
+        return st
